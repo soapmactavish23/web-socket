a716d9d968c75476c9d6022921a46986b749a68e
soapmactavish23 <ricknogueira1231@gmail.com>
2026-01-17 17:25:11 -0300

    Armazenando QR Code e criando rota de exibição de imagens

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, jsonify, request
+
+from payments.pix import Pix
 from repository.database import db
 from db_models.payment import Payment
 from datetime import datetime, timedelta
@@ -19,6 +21,11 @@ def create_payment_pix():
     expiration_date = datetime.now() + timedelta(minutes=30)
 
     new_payment = Payment(value=data['value'], expiration_date=expiration_date)
+
+    pix_obj = Pix()
+    data_payment_pix = pix_obj.create_payment()
+    new_payment.bank_payment_id = data_payment_pix["bank_payment_id"]
+    new_payment.qr_code = data_payment_pix["qr_code_path"]
 
     db.session.add(new_payment)
     db.session.commit()
