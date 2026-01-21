6d21b5e23250b5ae5c9fbdcf830bc782826b9c7b
soapmactavish23 <ricknogueira1231@gmail.com>
2026-01-21 19:41:50 -0300

    Notificando o cliente com pagamento confirmado

--- app.py
+++ app.py
@@ -59,12 +59,20 @@ def pix_confirmation():
 
     payment.paid = True
     db.session.commit()
-
+    socketio.emit(f'payment-confirmed-{payment.id}')
     return jsonify({"message": "The payment has been confirmed"}), 200
 
 @app.route("/payments/pix/<int:payment_id>", methods=["GET"])
 def payment_pix_page(payment_id):
     payment = Payment.query.get(payment_id)
+
+    if payment.paid:
+        return render_template(
+            'confirmed_payment.html',
+            payment_id=payment.id,
+            value=payment.value
+        )
+
     return render_template(
         'payment.html',
         payment_id=payment.id,
