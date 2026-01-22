dadb8fb6f19b5b083ee00426720f91a1a50f911d
soapmactavish23 <ricknogueira1231@gmail.com>
2026-01-22 19:48:48 -0300

    Criando Testes Unitários Para a Classe do Pix

---
+++ tests/test_pix.py
@@ -0,0 +1,21 @@
+import sys
+sys.path.append('../')
+
+import pytest
+import os
+from payments.pix import Pix
+
+def test_pix_create_payment():
+    pix_instance = Pix()
+
+    # create a payment
+    payment_info = pix_instance.create_payment()
+
+    assert "bank_payment_id" in payment_info
+    assert "qr_code_path" in payment_info
+
+    qr_code_path = payment_info["qr_code_path"]
+
+    assert os.path.exists(f"static/img/{qr_code_path}.png")
+
+    print(payment_info)
